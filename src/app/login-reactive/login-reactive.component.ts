2eae474a34ee1cef9b9313c2a779c757cc9fc72c
Nico Neiman <niconeiman@gmail.com>
2021-09-01 13:32:43 -0500

    validation

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -9,18 +9,39 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginReactiveComponent implements OnInit {
 
-    form: FormGroup = new FormGroup({
-      email: new FormControl('', {validators: [Validators.required, Validators.email]}),
-      password: new FormControl('', {validators: [Validators.required, Validators.minLength(8)]}),
-    });
+  // Using FormControl
 
-  constructor() {
+    //email: FormControl = new FormControl(
+    //   '',
+    //   {validators: [Validators.required, Validators.email], updateOn: 'blur'},
+    // );
+    // password: FormControl =  new FormControl(
+    //   '',
+    //   {validators: [Validators.required, Validators.minLength(8)]}
+    // );
+
+  // Using FormBuilder
+
+    form: FormGroup = this.fb.group({
+      email: ['', {validators: [Validators.required, Validators.email], updateOn: 'blur'}],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+
+  constructor(private fb: FormBuilder) {
 
 
   }
 
   ngOnInit() {
 
+  }
+
+  get email() {
+    return this.form?.controls['email'];
+  }
+
+  get password() {
+    return this.form?.controls['password'];
   }
 
 }
