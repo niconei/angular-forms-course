92de80a46f023e117d60b66a39da048bf2bcc5a9
Nico Neiman <niconeiman@gmail.com>
2021-09-02 11:51:04 -0500

    step 2

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,44 +1,55 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CoursesService} from '../../services/courses.service';
-import {Observable} from 'rxjs';
-import {filter} from 'rxjs/operators';
-import { courseTitleValidator } from '../../validators/course-title.validator';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { CoursesService } from "../../services/courses.service";
+import { Observable } from "rxjs";
+import { courseTitleValidator } from "../../validators/course-title.validator";
 
+interface CourseCategory {
+  code: string;
+  description: string;
+}
 @Component({
-  selector: 'create-course-step-1',
-  templateUrl: './create-course-step-1.component.html',
-  styleUrls: ['./create-course-step-1.component.scss']
+  selector: "create-course-step-1",
+  templateUrl: "./create-course-step-1.component.html",
+  styleUrls: ["./create-course-step-1.component.scss"],
 })
 export class CreateCourseStep1Component implements OnInit {
-
   form = this.fb.group({
-    title: ['', {
-      validators: [Validators.required, Validators.minLength(5), Validators.maxLength(60)],
-      asyncValidators: [courseTitleValidator(this.courses)],
-      updateOn: 'blur'
-    }],
+    title: [
+      "",
+      {
+        validators: [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(60),
+        ],
+        asyncValidators: [courseTitleValidator(this.courses)],
+        updateOn: "blur",
+      },
+    ],
     releaseDateAt: [new Date(), Validators.required],
     downloadsAllowed: [false, [Validators.requiredTrue]],
-    textAreaDescription: ['', [Validators.required, Validators.minLength(3)]]
-  })
-  constructor(private fb: FormBuilder, private courses: CoursesService) {
+    textAreaDescription: ["", [Validators.required, Validators.minLength(3)]],
+    category: ["BEGINNER", Validators.required ]
+  });
 
-  }
+  courseCategories$: Observable<CourseCategory>;
+
+  constructor(private fb: FormBuilder, private courses: CoursesService) {}
+
   ngOnInit() {
-
+    this.courseCategories$ = this.courses.findCourseCategories();
   }
 
   get courseTitle() {
-    return this.form?.controls['title'];
+    return this.form?.controls["title"];
   }
 
   get textArea() {
-    return this.form?.controls['textAreaDescription'];
+    return this.form?.controls["textAreaDescription"];
   }
 
   get downloadsAllowed() {
-    return this.form?.controls['downloadsAllowed'];
+    return this.form?.controls["downloadsAllowed"];
   }
-
 }
--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,19 +1,29 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'create-course-step-2',
-  templateUrl: 'create-course-step-2.component.html',
-  styleUrls: ['create-course-step-2.component.scss']
+  selector: "create-course-step-2",
+  templateUrl: "create-course-step-2.component.html",
+  styleUrls: ["create-course-step-2.component.scss"],
 })
 export class CreateCourseStep2Component implements OnInit {
+  form = this.fb.group({
+    courseType: ["premium", Validators.required],
+    price: [0, [Validators.required, Validators.min(1), Validators.max(9999), Validators.pattern("[1-9]+")]]
+  });
 
-
+  constructor(private fb: FormBuilder) {}
   ngOnInit() {
-
-
-
+    this.form.valueChanges.subscribe(
+      value => {
+        const priceControl = this.form.controls['price'];
+        if (value.courseType === 'free' && priceControl.enabled) {
+          priceControl.disable({emitEvent: false}); //Prevent infinte loop
+        }
+        else if (value.courseType === 'premium' && priceControl.disabled) {
+          priceControl.enable();
+        }
+      }
+    );
   }
-
 }
