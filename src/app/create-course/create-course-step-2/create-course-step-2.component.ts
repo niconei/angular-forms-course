a9435bfd58143e66804087e50b5bd4a2190116e2
Nico Neiman <niconeiman@gmail.com>
2021-09-02 12:11:49 -0500

    date range

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { createPromoRangeValidator } from "../../validators/date-range.validator";
 
 @Component({
   selector: "create-course-step-2",
@@ -7,23 +8,34 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
   styleUrls: ["create-course-step-2.component.scss"],
 })
 export class CreateCourseStep2Component implements OnInit {
-  form = this.fb.group({
-    courseType: ["premium", Validators.required],
-    price: [0, [Validators.required, Validators.min(1), Validators.max(9999), Validators.pattern("[1-9]+")]]
-  });
+  form = this.fb.group(
+    {
+      courseType: ["premium", Validators.required],
+      price: [
+        0,
+        [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(9999),
+          Validators.pattern("[1-9]+"),
+        ],
+      ],
+      dateRangeStart: [null],
+      dateRangeEnd: [null],
+    },
+    { validators: [createPromoRangeValidator()], updateOn: "blur" }
+  );
 
   constructor(private fb: FormBuilder) {}
+
   ngOnInit() {
-    this.form.valueChanges.subscribe(
-      value => {
-        const priceControl = this.form.controls['price'];
-        if (value.courseType === 'free' && priceControl.enabled) {
-          priceControl.disable({emitEvent: false}); //Prevent infinte loop
-        }
-        else if (value.courseType === 'premium' && priceControl.disabled) {
-          priceControl.enable();
-        }
+    this.form.valueChanges.subscribe((value) => {
+      const priceControl = this.form.controls["price"];
+      if (value.courseType === "free" && priceControl.enabled) {
+        priceControl.disable({ emitEvent: false }); //Prevent infinte loop
+      } else if (value.courseType === "premium" && priceControl.disabled) {
+        priceControl.enable();
       }
-    );
+    });
   }
 }
